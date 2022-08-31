da4bd8d81ac8677af9a53313ddcea4913ed631b0
SergeyChernyaev <chernayev.sergey.work@gmail.com>
2022-08-31 21:46:50 +0300

    Solved The Most Frequent

--- Initiation/the_most_frequent.py
+++ Initiation/the_most_frequent.py
@@ -10,15 +10,28 @@
 # END_DESC
 
 
+from collections import Counter
 
 def most_frequent(data: list) -> str:
     """
         determines the most frequently occurring string in the sequence.
     """
-   
+    c = Counter(data)
+    return c.most_common(1)[0][0]
+
+def most_frequent_map(data: list) -> str:
+    d = []
+    for i in data:
+        num = sum(list(map(lambda x: int(x==i), data)))
+        d.append((i, num))
+        
+    return sorted(d, key=lambda x: x[1])[1][0]
+  
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
+
+    
     print('Example:')
     print(most_frequent([
         'a', 'b', 'c', 

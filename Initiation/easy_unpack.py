593e6791429b9146e445044ae1258ab3ea32b87c
SergeyChernyaev <chernayev.sergey.work@gmail.com>
2022-09-01 20:35:34 +0300

    Solved Easy Unpack

--- Initiation/easy_unpack.py
+++ Initiation/easy_unpack.py
@@ -17,7 +17,9 @@ def easy_unpack(elements: tuple) -> tuple:
     """
         returns a tuple with 3 elements - first, third and second to the last
     """
-    
+    return (elements[0], elements[2], elements[-2])
+
+
 if __name__ == '__main__':
     print('Examples:')
     print(easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)))

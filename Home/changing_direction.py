2cf2c1d7523a04c99a4d8643a88050ec5a947fb6
Sergey Chernayev <chernayev.sergey.work@gmail.com>
2022-09-29 17:33:55 +0200

    Solved Changing Direction

--- Home/changing_direction.py
+++ Home/changing_direction.py
@@ -21,11 +21,21 @@
 
 def changing_direction(elements: list) -> int:
     # your code here
-    return 0.0
+    res = ''
+    for x, y in zip(elements, elements[1:]):
+        if y-x > 0:
+            res += '1'
+        elif y-x == 0:
+            res += '0'
+        else:
+            res += '2'
+    res = res.strip('0')
+    print(res)
+    return list(map(lambda x, y: abs(int(y)-int(x)), res, res[1:])).count(1)
 
 
 print("Example:")
-print(changing_direction([1, 2, 3, 4, 5]))
+print(changing_direction([1, 2, 2, 1, 2, 2]))
 
 assert changing_direction([1, 2, 3, 4, 5]) == 0
 assert changing_direction([1, 2, 3, 2, 1]) == 1
